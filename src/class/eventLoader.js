d9739afcd5935f754e6d8964277569c9ae65e795
zechaos031 <delrieu.adrien@gmail.com>
2021-10-22 11:37:56 +0200

    :wrench: Fix event adding

--- src/class/eventLoader.js
+++ src/class/eventLoader.js
@@ -5,7 +5,8 @@ module.exports = async function EventLoader() {
   const files = await fs.readdir(resolve("src/event"));
   const events = new Map();
   for await (const file of files) {
-    const event = require(`./src/event/${ file }`);
+    console.log(files,file)
+    const event = require("../event/"+file);
     events.set(event.name, event);
   }
   return events;
