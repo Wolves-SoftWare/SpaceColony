702fc43a8e1d6a97f3f14306dc926ca4d2f21fc0
zechaos031 <delrieu.adrien@gmail.com>
2021-10-24 01:03:56 +0200

    :wrench: Remove debug

--- src/class/eventLoader.js
+++ src/class/eventLoader.js
@@ -5,7 +5,6 @@ module.exports = async function EventLoader() {
   const files = await fs.readdir(resolve('src/event'));
   const events = new Map();
   for await (const file of files) {
-    console.log(files,file)
     const event = require('../event/'+file);
     events.set(event.name, event);
   }
