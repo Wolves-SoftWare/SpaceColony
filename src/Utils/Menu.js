511e71d0c4be9e17788da4878077cc5780fb94ce
zechaos031 <delrieu.adrien@gmail.com>
2021-10-24 01:05:18 +0200

    Update Menu.js

--- src/Utils/Menu.js
+++ src/Utils/Menu.js
@@ -9,13 +9,14 @@ class Menu {
 
   async primaryMenu(options = {}){
     Object.assign(options,this.option)
-    await this.game.menu(this.game, ['Building', 'Colonist', 'Research','Ressources'],options)
+    await this.game.menu(this.game, ['Building', 'Colonist', 'Research','Ressources','End Turn'],options)
   }
   async colonSummary(colon, options = {}){
     Object.assign(options,this.option)
     const skills = Object.keys(colon.skill)
     let skillSTR =''
     let relaSTR = ''
+    let taskSTR = ''
     this.game.terminal('\n\n')
     for(const skill of skills){
       skillSTR +=`\n\t${skill}: ${colon.skill[skill].skill}`
@@ -24,25 +25,36 @@ class Menu {
       if(!relation) return
       relaSTR +=`\n\t${relation.name}: ${relation.point}`
     }
+    for(const task of colon.tasks){
+      if(!task) return
+      relaSTR +=`\n\t${task}`
+    }
     this.game.terminal('Skill')
-
     this.game.terminal(skillSTR)
     this.game.terminal('\n\n')
     this.game.terminal('Relation')
-
     this.game.terminal(relaSTR)
-
-    await this.game.menu(this.game, ['Back'],options)
+    this.game.terminal('\n\n')
+    this.game.terminal('Task')
+    this.game.terminal(taskSTR)
+    this.game.terminal('\n\n')
+    this.game.colony.setColon(colon)
+    await this.game.menu(this.game, ['Back','Assign'],options)
   }
-
-
   async ColonistMenu(options = {}){
     Object.assign(options,this.option)
 
     const colony = this.game.colony.callColony
-    const colonistList = colony.colons.map(c => c.name)
+    const colonistList = Object.keys(colony.colons).map(c => c.name)
     colonistList.push('Back')
     await this.game.menu(this.game, colonistList,options)
+
+  }
+  async assignJob(options = {}){
+    let colon = this.game.colony.getColon
+    let job = ['woodcutter','hunt','craft']
+    await this.game.menu(this.game, job,options)
+    return console.log(colon)
   }
 }
 
