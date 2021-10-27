f89dc098bf455451200cd9bfba3cdeb8407368ba
zechaos031 <delrieu.adrien@gmail.com>
2021-10-27 17:08:27 +0200

    :wrench: Assign and Unassign

--- src/event/assignJob.js
+++ src/event/assignJob.js
@@ -1,11 +1,20 @@
 module.exports = {
   name: 'assignJob',
   func: async (game,colon,job) => {
-    colon.tasks.push(job);
+    if(!colon.tasks.includes(job)){
+      colon.tasks.push(job);
+      let data = game.colony.callColony
+      Object.assign(data.colons[colon.name],colon)
+      game.emit('colonist',colon,{clearTerminal:true})
+    }else {
 
-    let data = game.colony.callColony
-    Object.assign(data.colons[colon.name],colon)
-    game.emit('colonist',colon,{clearTerminal:true})
+      colon.tasks.splice(colon.tasks.indexOf(job), 1)
+      let data = game.colony.callColony
+      Object.assign(data.colons[colon.name],colon)
+      game.emit('colonist',colon,{clearTerminal:true})
+    }
+
+
 
   }
 }
