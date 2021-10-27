b21143fd6b499b6c04a99674fd92fd75f943e751
zechaos031 <delrieu.adrien@gmail.com>
2021-10-27 19:19:00 +0200

    :wrench: Change skill generation

--- src/Gen/Colonist.js
+++ src/Gen/Colonist.js
@@ -1,5 +1,3 @@
-let interrestRemain = 2
-let skillRemain = 2
 const fs = require('fs/promises');
 module.exports = {
   generate(size) {
@@ -18,6 +16,7 @@ module.exports = {
         }
       })
     }
+
     for ( const colon of Object.keys(colonists) ) {
       this.generateSocial(colonists[colon],colonists)
     }
@@ -30,7 +29,6 @@ module.exports = {
         turn:0
       }
     }
-
     fs.writeFile('./src/data/game/game.json', JSON.stringify(data,null,2))
   },
 
@@ -42,8 +40,6 @@ module.exports = {
     return {gender: selectGender,name: selectGender === 'M' ? male[ Math.floor(Math.random() * male.length)] : female[ Math.floor(Math.random() * female.length)] }
   },
   generateSkill(){
-    interrestRemain = 2
-    skillRemain = 2
     return {
       farming: this.generatePoints(),
       extract: this.generatePoints(),
@@ -55,49 +51,27 @@ module.exports = {
     }
   },
   generatePoints(){
-    let skill = Math.floor(Math.random() * 20) + 1
     let interest = Math.floor(Math.random() * 3)
-    let xp = (skill*1000)
 
-
-    //Skill nerf
-    if(skillRemain !== 0){
-      if(skill > 10){
-        skillRemain--
-      }else {
-        skill =Math.floor(Math.random() * 9) + 1
-        xp = (skill*1000)
-      }
-    }else {
-      skill =Math.floor(Math.random() * 9) + 1
-      xp = (skill*1000)
-    }
-
-    //Interest Nerf
-    if(interrestRemain !== 0) {
-      if ( interest === 2 ) {
-        interrestRemain--
-      } else {
-        interest = Math.floor(Math.random() * 2)
-      }
-    } else {
-      interest = Math.floor(Math.random() * 2)
-    }
-
+    //calcul des points
+    let skillLvl = require('../Utils/Utils').choice( [1,2,3,4,5,6 ,7 ,8 ,9,10,11,12,13,14,15,16,17,18,19,20],[7,7,7,8,10,15,12,12,9 ,7 ,6 ,5 ,4, 4 ,3 ,2 ,2 ,1, 1] )
+    interest =  require('../Utils/Utils').choice( [0,1,2],[20,7,1],{allowZero:false} )
+    let modificator = interest === 1 ? 1.4 : interest === 2 ? 1.5 : 1
+    let xp = parseInt(skillLvl*1000 + (skillLvl-1)*200/modificator)
     return{
-      skill,
+      skillLvl,
       interest,
       xp
     }
   },
 
   generateSocial(colon, colonList){
-    let chance = Math.floor(Math.random() * 10)+1
-    console.log(chance,colonList);
-    if(chance === 10){
+    let chance = true
+    if(chance){
       let list = Object.keys(colonList)
       let colon2 = list.filter(c => c !== colon.name)[Math.floor(Math.random() * list.length)]
       let point = Math.floor(Math.random() * 125) -75
+      if(!colonList[colon2]) return // random error
       colon.social.push({
         name:colonList[colon2].name,
         point,
