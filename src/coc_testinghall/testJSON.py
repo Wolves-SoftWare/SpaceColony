31edd5a74cd9e5b1ac0b0d8e9d8eed7740049bb3
Florian Delrieu <florian.delrieu@free.fr>
2021-10-30 11:06:29 +0200

    JSON saves in progress

--- src/coc_testinghall/testJSON.py
+++ src/coc_testinghall/testJSON.py
@@ -1,32 +1,37 @@
+"""
+TODO
+    - Faire une boucle pour convertir tout les elements d'une liste qui sont dans[Planet,Star,Satellite,System,Orbit]
+"""
+
 import json
 from src.SSG.Class.Planet import Planet
-
+from src.SSG.Class.System import System
+from src.SSG.Class.Star import Star
+from src.SSG.Class.Satellite import Satellite
+from src.SSG.Class.Orbit import Orbit
+import os
+os.chdir("../SSG")
+path = "/Users/floriandelrieu/OneDrive/Logiciels et Jeux/SpaceColony/src/coc_testinghall/"
 my_details = {
     'name': 'John Doe',
-    'age': 29
+    'age' : 29
 }
+
 P1 = Planet(Name="TestPlanet")
-P1.Satellites_list = ["AST1","AST2"]
-P1.MineralSurvey = {
-    "Minerals": 4,
-    "Common": 2
-}
-P = Planet().__dict__
-del(P["Satellites_list"])
-del(P["MineralSurvey"])
-
-
-#with open('test.json', 'w') as json_file:
-#    json.dump(P, json_file)
-
+S1 = System()
 
 def SaveInJSON(Planet,json_name):
     dico = Planet.__dict__
-    for k in list(dico.keys()):
-        if type(dico[k]) is (dict or list):
-            json_file = open(json_name+".json","w")
-            json.dump(dico[k], json_file)
-    json_file = open(json_name+".json","w")
+    json_file = open(path + json_name+".json","w")
     json.dump(dico, json_file)
 
-SaveInJSON(P1,"P1")
+def ConvertInDict(object):
+    dico = object.__dict__
+    for k in list(dico.keys()):
+        if type(dico[k]) in [Planet,Star,Satellite,System,Orbit]:
+            dico[k] = dico[k].__dict__
+        elif type(dico[k]) is list:
+            for el in dico[k]:
+                if type(el) in [Planet,Star,Satellite,System,Orbit]:
+                    el = el.__dict__
+SaveInJSON(S1,"S1")
