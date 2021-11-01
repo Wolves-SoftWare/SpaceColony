88d0ebf36070f73b81fd5823af79e42a1ece4df8
zechaos031 <delrieu.adrien@gmail.com>
2021-11-01 12:31:27 +0100

    :wrench: add faune get

--- src/class/Entity.js
+++ src/class/Entity.js
@@ -1,12 +1,20 @@
 class Entity{
   constructor() {
     this.items = require('../data/assets/items.json')
+    this.faune = require('../data/assets/faune.json')
+
   }
 
   getItem(name){
     if(!name) return 'Pas d\'item trouvé'
     if(!this.items[name]) return 'Pas d\'item trouvé'
     return this.items[name]
+  }
+
+  getFaune(name){
+    if(!name) return 'Pas d\'annimaux trouvé'
+    if(!this.faune[name]) return 'Pas d\'annimaux trouvé'
+    return this.faune[name]
   }
 }
 
