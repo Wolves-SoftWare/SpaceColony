3e6a632333fb9ae7d9bf63d68846ec7c9de65839
zechaos031 <delrieu.adrien@gmail.com>
2021-11-11 20:14:49 +0100

    :wrench: begin generator and refactor move

--- Game/game.cs
+++ Game/game.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System.Collections;
 using SpaceColony.Script.Utils;
+using SpaceColony.Modules.Colonist;
 
 public class game : Node
 {
@@ -11,15 +12,7 @@ public class game : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		ArrayList List = new ArrayList();
-		ArrayList Weights = new ArrayList();
-		List.Add("Hello,");
-		List.Add("World,");
-		List.Add("!");
-		Weights.Add(1);
-		Weights.Add(0);
-		Weights.Add(2);
-		GD.Print(Choice.Make(List,Weights));
+		Generator.generate(5);
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
--- Script/Utils/Choice.cs
+++ Script/Utils/Choice.cs
@@ -1,36 +1,36 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 namespace SpaceColony.Script.Utils
 
 {
-    public class Choice
-    {
-        public static System.Object Make(ArrayList list,ArrayList weights)
-        {
-            ArrayList newList = new ArrayList();
-            ArrayList newWeights = new ArrayList();
-            for (int i = 0; i < weights.Count; i++)
-            {
-                int number = Convert.ToInt32(weights[i]);
-                if (number != 0)
-                {
-                    newList.Add(list[i]);
-                    newWeights.Add(weights[i]);
-                }
-            }
-            ArrayList chance = new ArrayList();
-            for (int i = 0; i < newList.Count; i++)
-            {
-                int indice = int.Parse(newWeights[i].ToString());
-                for (int l = 0; l < indice; l++)
-                {
-                    chance.Add(newList[i]);
-                }
-            }
-            Random random = new Random();
-            return chance[random.Next(0, chance.Count)];
-        }
-    }
+	public class Choice
+	{
+		public static System.Object Make(ArrayList list,ArrayList weights)
+		{
+			ArrayList newList = new ArrayList();
+			ArrayList newWeights = new ArrayList();
+			for (int i = 0; i < weights.Count; i++)
+			{
+				int number = Convert.ToInt32(weights[i]);
+				if (number != 0)
+				{
+					newList.Add(list[i]);
+					newWeights.Add(weights[i]);
+				}
+			}
+			ArrayList chance = new ArrayList();
+			for (int i = 0; i < newList.Count; i++)
+			{
+				int indice = int.Parse(newWeights[i].ToString());
+				for (int l = 0; l < indice; l++)
+				{
+					chance.Add(newList[i]);
+				}
+			}
+			Random random = new Random();
+			return chance[random.Next(0, chance.Count)];
+		}
+	}
 }
